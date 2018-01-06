3cf4a6a427fae692a7a981e17dadc71bb4104fc6
Peter Kong <bansuki@gmail.com>
2018-01-05 21:34:05 -0600

    connected scraper code

--- search_recipes/recipe.py
+++ search_recipes/recipe.py
@@ -4,28 +4,28 @@ import urllib.request
 import json
 
 from .ingredients import extract_ingredient_data
+from .scrape_steps import lambda_handler as scrape_steps
 
 # hits edamam api for results
 # TODO consider having it hit our cache for search results and doing a map reduce
 # TODO in this case, the below method becomes generic and we pass an array of services to call
 def get_recipes(params):
     # TODO maybe do some NLP clean up and tagging of search terms here
-    query = params['q']
-    # TODO use pagination and default list size!
-    results = edamam_api(query)
+    results = search_edamam(params)
     # TODO filter search results by various params
     # TODO sort by various params criteria
     recipes = map(format_open_recipe, results)
     return list(recipes)
 
-def edamam_api(query):
-    params = urllib.parse.urlencode({
-        'q': query,
+def search_edamam(query_params):
+    default_query_params = {
         'app_id': os.environ['edamam_id'],
         'app_key': os.environ['edamam_key'],
         'from': 0,
-        'to': 10
-    })
+        'to': 1
+    }
+    final_query_params = {**default_query_params, **query_params}
+    params = urllib.parse.urlencode(final_query_params)
     url = os.environ['edamam_url'] + params
     response = urllib.request.urlopen(url).read()
     return json.loads(response)['hits']
@@ -33,6 +33,7 @@ def edamam_api(query):
 # takes json from edamam api and returns in open-recipe-format
 def format_open_recipe(edamam_json):
     src_json = edamam_json['recipe']
+    # TODO pull from cache if it exists
     ingredients = list(map(extract_ingredient_data, src_json['ingredients']))
     return {
         'recipe_name': src_json['label'],
@@ -42,5 +43,5 @@ def format_open_recipe(edamam_json):
             'unit': 'servings'
         },
         'ingredients': ingredients,
-        # 'steps': TODO call scrapely
+        'steps': scrape_steps({'url': src_json['url']}, {})
     }
--- search_recipes/scrape_steps.py
+++ search_recipes/scrape_steps.py
@@ -1,6 +1,10 @@
-# assumes event has body in open-recipe-format JSON
+from scrapely import Scraper
+s = Scraper()
+# assumes event member 'url' with value of url to be scraped for recipe steps
+# TODO make this its own lambda later!!
 def lambda_handler(event, context):
-    recipe = event.body
+    url = event['url']
+    steps = []
     # TODO iterate through possible fields and check value
     # TODO if required and invalid, add "incomplete" flag to recipe
     # TODO add missing fields by:
@@ -11,4 +15,4 @@ def lambda_handler(event, context):
     #  2. if training site also fails, retrain and save new scraper
     # retry scrape.
     # if retry fails, return empty object.
-    return recipe
+    return steps
