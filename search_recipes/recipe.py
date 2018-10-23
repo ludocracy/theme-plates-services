1fd05c816853c86985669157a4929919f1e0bec4
Rush <rush@Rushs-MacBook-Pro.local>
2018-10-22 21:13:16 -0500

    got tests running

--- search_recipes/recipe.py
+++ search_recipes/recipe.py
@@ -2,6 +2,7 @@ import os
 import urllib.parse
 import urllib.request
 import json
+from yaml import load  # TODO remove when we switch to DB!
 
 from .ingredients import extract_ingredient_data
 from .scrape_steps import lambda_handler as scrape_steps
@@ -20,32 +21,37 @@ def get_recipes(params):
 
 
 def search_edamam(query_params):
+    configs = load_api_configs()
     default_query_params = {
-        'app_id': os.environ['edamam_id'],
-        'app_key': os.environ['edamam_key'],
+        'app_id': configs['edamam_id'],
+        'app_key': configs['edamam_key'],
         'from': 0,
         'to': 1
     }
     final_query_params = {**default_query_params, **query_params}
     params = urllib.parse.urlencode(final_query_params)
-    url = os.environ['edamam_url'] + params
+    url = configs['edamam_url'] + params
     response = urllib.request.urlopen(url).read()
     return json.loads(response)['hits']
+
+
+def load_api_configs(config_file: str = None):
+    return load(config_file or '.edamam.config.yaml')
 
 # takes json from edamam api and returns in open-recipe-format
 
 
-def format_open_recipe(edamam_json):
-    src_json = edamam_json['recipe']
+def format_open_recipe(edamam_dict: dict):
+    src_dict = edamam_dict['recipe']
     # TODO pull from cache if it exists
-    ingredients = list(map(extract_ingredient_data, src_json['ingredients']))
+    ingredients = list(map(extract_ingredient_data, src_dict['ingredients']))
     return {
-        'recipe_name': src_json['label'],
-        'source_url': src_json['url'],
+        'recipe_name': src_dict['label'],
+        'source_url': src_dict['url'],
         'yields': {
-            'amount': src_json['yield'],
+            'amount': src_dict['yield'],
             'unit': 'servings'
         },
         'ingredients': ingredients,
-        'steps': scrape_steps({'url': src_json['url']}, {})
+        'steps': scrape_steps({'url': src_dict['url']}, {})
     }
